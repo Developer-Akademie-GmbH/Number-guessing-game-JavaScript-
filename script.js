cc1e5feaff812c63f6f4193185132b1ed34f7621
Junus Ergin <junus91@googlemail.com>
2024-06-06 08:46:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let numberToGuess = Math.round(Math.random() * 100);
+let tries = 0;
+
+function guessTheNumber() {
+    tries = tries + 1;
+    displayTries.innerHTML = 'Versuche: ' + tries;
+    if(numberToGuess == myNumber.value) {
+        headline.innerHTML = 'Du hast gewonnen!!! 🥳🎉';
+        let jsConfetti = new JSConfetti();
+        jsConfetti.addConfetti();
+    }
+    
+    if(numberToGuess > myNumber.value) {
+        headline.innerHTML = 'Die Zahl ist größer!';
+    }
+
+    if(numberToGuess < myNumber.value) {
+        headline.innerHTML = 'Die Zahl ist kleiner!';
+    }
+    myNumber.value = '';
+}
